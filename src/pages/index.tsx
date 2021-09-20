439451de6eb6cdea1920a6670b893044d30ac555
CarolinaScudeler <carolina.oliveira@bandtec.com.br>
2021-09-20 15:02:55 -0300

    Creating project and organizing

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <link rel="shortcut icon" href="carolina-scudeler.svg" />
+        <title>Portfólio | Carolina Scudeler</title>
+      </Head>
+      <main>
+        <h1>Hello World</h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
